a5a58981c0d0998964913f9ea1935369a9b36950
Danielztd1 <108360287+Danielztd1@users.noreply.github.com>
2023-10-20 14:29:27 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,12 +2,12 @@
 session_start();
 ob_start();
 include_once 'conexao.php';
-
+     
 if((!isset($_SESSION['id'])) AND (!isset($_SESSION['nome']))){
     $_SESSION['msg'] = "<p style='color: #ff0000'>Erro: Necessário realizar o login para acessar a página!</p>";
     header("Location: index.php");
 }
-
+    
 ?>
 
 <!DOCTYPE html>
